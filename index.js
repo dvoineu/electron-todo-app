6b64fbd982a78a128b795b9a6ec6244428add4fa
dvoineu <av.dvoinev@gmail.com>
2020-02-23 00:13:38 +0100

    Add new TODO Window

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const electron = require('electron');
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
@@ -11,11 +12,24 @@ app.on('ready', () => {
   Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add new TODO'
+  });
+}
+
 const menuTemplate = [
   {
     label: 'File',
     submenu: [
-      { label: 'New Todo' },
+      {
+        label: 'New Todo',
+        click() {
+          createAddWindow();
+        }
+      },
       {
         label: 'Quit',
         accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
